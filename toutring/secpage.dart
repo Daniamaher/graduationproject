e75f2d3b930520244ba0c1a8f616e7dcbb2ef358
Maha Ayman <142231792+MahaAyman1@users.noreply.github.com>
2024-05-03 18:52:44 +0300

    Add files via upload

---
+++ toutring/secpage.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class FormPage2 extends StatefulWidget {
+  @override
+  _FormPage2State createState() => _FormPage2State();
+}
+
+class _FormPage2State extends State<FormPage2> {
+  String? selectedCategory;
+  String? selectedSubcategory;
+  List<String> subjectsToTeach = [];
+  bool wantsToTeach = false;
+
+  Map<String, Map<String, List<String>>> categoryMap = {
+    'Computer and Information Technology': {
+      'Computer Science': [
+        'Algorithms and Data Structures',
+        'Computer Architecture',
+        'Operating Systems',
+        'Database Systems',
+        'C++'
+      ],
+      'Information Technology': [
+        'Subject 4',
+        'Subject 5',
+        'Subject 6',
+      ],
+    },
+    'Engineering': {
+      'Mechanical Engineering': [
+        'Subject A',
+        'Subject B',
+        'Subject C',
+      ],
+      'Electrical Engineering': [
+        'Subject X',
+        'Subject Y',
+        'Subject Z',
+      ],
+    },
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE6F3F3),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 50),
+                DropdownButtonFormField<String>(
+                  value: selectedCategory,
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedCategory = newValue;
+                      selectedSubcategory = null; // Reset subcategory
+                      subjectsToTeach.clear(); // Clear selected subjects
+                    });
+                  },
+                  items: categoryMap.keys.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  hint: Text('Your department is '),
+                ),
+                const SizedBox(height: 20),
+                DropdownButtonFormField<String>(
+                  value: selectedSubcategory,
+                  onChanged: (newValue) {
+                    setState(() {
+                      selectedSubcategory = newValue;
+                      subjectsToTeach.clear(); // Clear selected subjects
+                    });
+                  },
+                  items: getSubcategories().map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  hint: Text('Your major is '),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: wantsToTeach,
+                      onChanged: (value) {
+                        setState(() {
+                          wantsToTeach = value!;
+                        });
+                      },
+                    ),
+                    Text('I want to teach'),
+                  ],
+                ),
+                if (wantsToTeach &&
+                    selectedCategory != null &&
+                    selectedSubcategory != null)
+                  Column(
+                    children: [
+                      Text('Subjects in ${selectedSubcategory!}:'),
+                      SizedBox(height: 10),
+                      Wrap(
+                        children: getSubjects().map((subject) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: FilterChip(
+                              label: Text(subject),
+                              selected: subjectsToTeach.contains(subject),
+                              onSelected: (isSelected) {
+                                setState(() {
+                                  if (isSelected) {
+                                    subjectsToTeach.add(subject);
+                                  } else {
+                                    subjectsToTeach.remove(subject);
+                                  }
+                                });
+                              },
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  child: Text(
+                    'Finish',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Color(0xFFE6F3F3),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF111236),
+                  ),
+                  onPressed: () {
+                    // Handle finish button press
+                    Navigator.of(context).pushNamed('ProfileScreen');
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<String> getSubcategories() {
+    if (selectedCategory != null) {
+      return categoryMap[selectedCategory!]!.keys.toList();
+    }
+    return [];
+  }
+
+  List<String> getSubjects() {
+    if (selectedCategory != null && selectedSubcategory != null) {
+      return categoryMap[selectedCategory!]![selectedSubcategory!]!;
+    }
+    return [];
+  }
+}
---
+++ toutring/tout.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:front/collectinfo/firstpage.dart';
+import 'package:front/home.dart';
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1485290334039-a3c69043e517?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYyOTU3NDE0MQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300'),
+            ),
+            accountEmail: Text('Maha@example.com'),
+            accountName: Text(
+              'Maha Ayman',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.black87,
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.house),
+            title: const Text(
+              'Home',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const Home(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: const Text(
+              'Profile',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const FormPage1(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text(
+              'Setting',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => const FormPage1(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
